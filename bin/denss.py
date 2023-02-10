93ccb5c72625a0c0546ca915e5fe202560313251
AlfredoBellisario <36385500+AlfredoBellisario@users.noreply.github.com>
2023-02-10 15:46:55 +0100

    adding argument dark_reference

--- bin/denss.py
+++ bin/denss.py
@@ -68,6 +68,8 @@ if __name__ == "__main__":
         ne=args.ne,
         voxel=args.voxel,
         oversampling=args.oversampling,
+        rho_start=args.rho_start, 
+        dark_reference=args.dark_reference,
         recenter=args.recenter,
         recenter_steps=args.recenter_steps,
         recenter_mode=args.recenter_mode,
