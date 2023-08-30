288d7f9dc5134953a36a724731a8dadb1ed42e1d
Alfredo Bellisario <36385500+AlfredoBellisario@users.noreply.github.com>
2023-08-30 16:58:22 +0200

    Removed unnecessary print() from saxstats.py

--- saxstats/saxstats.py
+++ saxstats/saxstats.py
@@ -1177,8 +1177,6 @@ def denss(q, I, sigq, dmax, qraw=None, Iraw=None, sigqraw=None,
     if dark_support is not None:
         if dark_reference is not None:
             support = dark_support.copy()
-            print(support)
-            print(support.dtype)
         else:
             print('No densities defined when using dark support! Either pass dark_densities or use support')
         
